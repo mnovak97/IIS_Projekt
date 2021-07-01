aaab6dd82f2b782a60d6aade1f93af0c2e4e4e7d
mnovak97 <63095399+mnovak97@users.noreply.github.com>
2021-07-01 18:39:49 +0200

    SOPA and XPath

---
+++ SOAP_servis/SOAP_servis/FinderServis.asmx.cs
@@ -0,0 +1,61 @@
+﻿using SOAP_servis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+
+namespace SOAP_servis
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class FinderServis : System.Web.Services.WebService
+    {
+        internal static List<Kupac> PopisKupaca;
+        XmlDocument xmlDoc;
+
+        public FinderServis()
+        {
+           
+            PopisKupaca = new List<Kupac>();
+            Kupac k1 = new Kupac("Martin", "ipad3monkey@gmail.com", "0919158566", "Gornji Bukovac 96", "12345678941");
+            Kupac k2 = new Kupac("Marko", "marko@gmail.com", "0994569874", "Ilica 242", "45621478963");
+            PopisKupaca.Add(k1);
+            PopisKupaca.Add(k2);
+            xmlDoc = new XmlDocument();
+            XmlElement root = xmlDoc.CreateElement("Kupci");
+            xmlDoc.AppendChild(root);
+            foreach (var kupac in PopisKupaca)
+            {
+                XmlElement child = xmlDoc.CreateElement("Kupac");
+                XmlElement ime = xmlDoc.CreateElement("Ime");
+                ime.InnerText = kupac.Ime;
+                child.AppendChild(ime);
+                XmlElement email = xmlDoc.CreateElement("Email");
+                email.InnerText = kupac.Email;
+                child.AppendChild(email);
+                XmlElement brojMobitela = xmlDoc.CreateElement("BrojMobitela");
+                brojMobitela.InnerText = kupac.BrojMobitela;
+                child.AppendChild(brojMobitela);
+                XmlElement adresa = xmlDoc.CreateElement("Adresa");
+                adresa.InnerText = kupac.Adresa;
+                child.AppendChild(adresa);
+                XmlElement oib = xmlDoc.CreateElement("OIB");
+                oib.InnerText = kupac.OIB;
+                child.AppendChild(oib);
+                root.AppendChild(child);
+            }
+        }
+        [WebMethod]
+        public string Find(string word)
+        {
+            XmlNode kupac = xmlDoc.SelectSingleNode("/Kupci/Kupac[Ime='"+word+"']");
+
+            return kupac.InnerText.ToString();
+
+        }
+    }
+}
---
+++ SOAP_servis/SOAP_servis/Models/Kupac.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOAP_servis.Models
+{
+    [Serializable]
+    public class Kupac
+    {
+        public string Ime { get; set; }
+        public string Email { get; set; }
+        public string BrojMobitela { get; set; }
+        public string Adresa { get; set; }
+        public string OIB { get; set; }
+
+        public Kupac(string ime,string email,string brojMobitela,string adresa,string oib)
+        {
+            Ime = ime;
+            Email = email;
+            BrojMobitela = brojMobitela;
+            Adresa = adresa;
+            OIB = oib;
+        }
+    }
+}
