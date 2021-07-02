39fcca3ec1fd0a72e34e0523a699a7eef68a2fe6
mnovak97 <63095399+mnovak97@users.noreply.github.com>
2021-07-02 20:48:40 +0200

    XML-RPC service

---
+++ XML-RPC/src/xml/rpc/XMLRPC.java
@@ -0,0 +1,26 @@
+
+package xml.rpc;
+
+import java.io.IOException;
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.server.PropertyHandlerMapping;
+import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
+import org.apache.xmlrpc.webserver.WebServer;
+
+public class XMLRPC {
+
+    public static void main(String[] args) throws IOException, XmlRpcException {
+        
+        WebServer server = new WebServer(8080);
+        PropertyHandlerMapping phm = new PropertyHandlerMapping();
+        phm.addHandler("Temperatura", Temperatura.class);
+        server.getXmlRpcServer().setHandlerMapping(phm);
+        
+        XmlRpcServerConfigImpl serverConfig = (XmlRpcServerConfigImpl) server.getXmlRpcServer().getConfig();
+        serverConfig.setEnabledForExtensions(true);
+        
+        
+        server.start();
+    }
+    
+}
